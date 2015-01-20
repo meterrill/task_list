d5993616109db85cb005b4a3e332cd3314776c3a
Matthew Terrill <meterrill@gmail.com>
2015-01-20 11:13:50 -0800

    add database connection to spec

--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -41,3 +41,11 @@ describe(Todo) do
     end
   end
 end
+
+DB = PG.connect ({:dbname => 'todo_test'})
+
+RSpec.configure do |config|
+  config.after(:each) do
+    DB.exec("DELETE FROM description *;")
+  end
+end
