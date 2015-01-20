2ffdd903d5585409f775139ad4754380f590f58a
Matthew Terrill <meterrill@gmail.com>
2015-01-20 10:56:32 -0800

    add #id

--- lib/to_do.rb
+++ lib/to_do.rb
@@ -1,13 +1,11 @@
 class Todo
 
+  attr_reader(:id, :description)
   @@todo_list = []
 
   define_method(:initialize) do |attributes|
     @description = attributes.fetch(:description)
-  end
-
-  define_method(:description) do
-    @description
+    @id = attributes.fetch(:id)
   end
 
   define_singleton_method(:all) do
--- spec/to_do_spec.rb
+++ spec/to_do_spec.rb
@@ -2,10 +2,22 @@ require('rspec')
 require('to_do')
 
 describe(Todo) do
+
+  before() do
+    Todo.clear()
+  end
+
   describe("#description") do
     it("lets you give it a description") do
-      test_todo = Todo.new({:description => "scrub the zebra"})
+      test_todo = Todo.new({:id => 1, :description => "scrub the zebra"})
       expect(test_todo.description).to(eq("scrub the zebra"))
+    end
+  end
+
+  describe('#id') do
+    it('lets you give it an id') do
+      test_todo = Todo.new({:id => 1, :description => "scrub the zebra"})
+      expect(test_todo.id).to(eq(1))
     end
   end
 
@@ -17,7 +29,7 @@ describe(Todo) do
 
   describe('#save') do
     it('saves description') do
-      test_todo = Todo.new({:description => "scrub the zebra"})
+      test_todo = Todo.new({:id => 1, :description => "scrub the zebra"})
       test_todo.save()
       expect(Todo.all()).to(eq([test_todo]))
     end
@@ -25,9 +37,10 @@ describe(Todo) do
 
   describe('.clear') do
     it('clears the todo list array') do
-      test_todo = Todo.new({:description => "scrub the zebra"})
+      test_todo = Todo.new({:id => 1, :description => "scrub the zebra"})
       test_todo.save()
-      expect(Todo.clear()).to(eq([]))
+      Todo.clear()
+      expect(Todo.all()).to(eq([]))
     end
   end
 end
